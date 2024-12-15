a9735ebbee7fad999e136ded8ff1faa88380e5ed
Efiom Ndaeyo <rylshadow@yahoo.com>
2024-12-16 03:53:48 +0800

    Define models

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+    
+class Blogpost(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    video = models.FileField(upload_to='videos/')
+
+    def __str__(self):
+        return self.title
