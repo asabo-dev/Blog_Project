16034916c482c69585d532ea146015f54fbc7cef
Efiom Ndaeyo <rylshadow@yahoo.com>
2024-12-15 17:23:35 +0800

    Redefine Models

--- blogs/models.py
+++ blogs/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Topic(models.Model):
-    text = models.TextField(max_length=1000)
+    """A topic the blogger is writing about."""
+    text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
