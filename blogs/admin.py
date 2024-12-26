2d792aaf303d3a61e4aa200c4a7d0b9f9a9d4dd6
Efiom Ndaeyo <rylshadow@yahoo.com>
2024-12-26 14:45:46 +0800

    Update modified files

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Comment, Tag,
+from .models import Post, Category, Comment, Tag
 
 # Register your models here.
 @admin.register(Post)
