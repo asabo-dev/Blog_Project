2acd4b94b6c7ac95c57e65617c28e030a93b9e18
Efiom Ndaeyo <rylshadow@yahoo.com>
2024-12-26 12:48:55 +0800

    Define models

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+class Post(models.Model):
+    """Post model"""
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """"Return title"""
+        return self.title
