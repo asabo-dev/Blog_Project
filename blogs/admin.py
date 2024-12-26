4be704c23f1a383407421fc61e94b0b2ab2a7454
Efiom Ndaeyo <rylshadow@yahoo.com>
2024-12-26 14:14:39 +0800

    Fix admin.py modifications

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category, Comment, Tag,
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created_at')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
