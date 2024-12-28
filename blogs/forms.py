a09b71ca16e4f65da71c18a00741be61306f15b9
Efiom Ndaeyo <rylshadow@yahoo.com>
2024-12-28 18:01:02 +0800

    modify forms.py

--- blogs/forms.py
+++ blogs/forms.py
@@ -5,13 +5,13 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'text']
-        labels = {'title': '', 'text': ''}
-        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+        fields = ['title', 'content']
+        labels = {'title': '', 'content': ''}
+        widgets = {'content': forms.Textarea(attrs={'cols': 80})}
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['text']
-        labels = {'text': ''}
-        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+        fields = ['content']
+        labels = {'content': ''}
+        widgets = {'content': forms.Textarea(attrs={'cols': 80})}
